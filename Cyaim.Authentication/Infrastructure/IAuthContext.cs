339dfa482811c35334f5774b23efe1394e1442b4
云 残 <lbhdr@hotmail.com>
2021-06-12 23:42:56 +0800

    add auth context

---
+++ Cyaim.Authentication/Infrastructure/IAuthContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyaim.Authentication.Infrastructure
+{
+    /// <summary>
+    /// 鉴权上下文
+    /// </summary>
+    public interface IAuthContext
+    {
+    }
+}
